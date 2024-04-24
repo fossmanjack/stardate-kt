98faaa354d4c5cbc4113eecfa69478f453e313b3
jack <jack@sagephoenix.org>
2024-04-24 10:17:49 -0500

    Reformatting

--- stardate.kt
+++ stardate.kt
@@ -7,10 +7,38 @@ import java.math.RoundingMode
 import java.time.Instant
 import java.time.format.DateTimeParseException
 
+val EPOCH: Double = -740534400.0		// Unix epoch 1946-07-15T00:00:00.000T
+val MODULUS: Double = 31_558.1184		// 100y / 10000
+
+/**
+ *
+ */
+fun getStardateFromInstant(timestamp: Instant): Double {
+	var secs: Double = (timestamp.toEpochMilli() / 1000).toDouble()
+
+	var stardateEpochSecond: Double = secs - EPOCH
+	stardateEpochSecond /= MODULUS
+
+	return stardateEpochSecond.toBigDecimal().setScale(4, RoundingMode.HALF_UP).toDouble()
+}
+
+/**
+ *
+ */
+fun getInstantFromStardate(stardate: Double): Instant {
+	var ret: Double = stardate * MODULUS
+	ret += EPOCH
+
+	return Instant.ofEpochMilli(ret.toLong())
+
+	//throw Exception("Not yet implemented")
+}
+
+/**
+ *
+ */
 fun main(args: Array<String>) {
 	var timestamp: Instant
-	val epoch = -740534400 						// Unix epoch 1946-07-15
-	val modulus = 31_558.1184
 
 	if(args.size == 0) timestamp = Instant.now()
 	else
@@ -23,54 +51,4 @@ fun main(args: Array<String>) {
 
 	print(getStardateFromInstant(timestamp))
 }
-
-fun getStardateFromInstant(timestamp: Instant): Double {
-	var secs: Double = (timestamp.toEpochMilli() / 1000).toDouble()
-	val epoch = -740534400
-	val modulus = 31_558.1184
-
-	var stardateEpochSecond: Double = secs - epoch
-	stardateEpochSecond /= modulus
-
-	return stardateEpochSecond.toBigDecimal().setScale(4, RoundingMode.HALF_UP).toDouble()
-}
-
-fun getInstantFromStardate(stardate: Double): Instant {
-
-	return Instant.now()
-}
-
-/*
-fun main() {
-	var now = System.currentTimeMillis() / 1000
-	val epoch = -740534400 							// Unix epoch 1946-07-15
-	val modulus = 31_558.1184 						// 100y / 10_000
-
-	var epochTime = now - epoch
-	var stardateRaw = epochTime / modulus
-	val stardate = stardateRaw.toBigDecimal().setScale(3, RoundingMode.HALF_UP).toDouble()
-
-	print(stardate)
-
-
-
-}
-*/
-
-/* NOTES
-
-Algorithm:
-
-- Current time in ms (double?)
-- Subtract epoch timestamp in ms
-- Divide my modulus (average length-of-year in seconds / 1000 star"days" per year)
-
-I guess another way to do the rounding would be to multiply by the scale of the
-modulus (10_000 in this case), do the needed arithmetic, then divide by 1000
-
-This works though
-
- *
- *
-*/
 
